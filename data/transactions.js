ddf36e337fb3096772beac93b54fbaaba0ca68af
gurjot <gurjot.s0234@gmail.com>
2020-10-12 14:38:19 +0800

    Add files via upload
    Commit updated data transaction.js

--- data/transactions.js
+++ data/transactions.js
@@ -4,14 +4,14 @@
       fromAccount: "SV10002",
       toAccount: "HS10002",
       toUser: "Tom",
-      transactionDate: "05/24/20202",
+      transactionDate: "05/10/2020",
       amount: "1000"
     },
     {
       type: "One time",
       fromAccount: "SV10002",
-      toAccount: "TestAccount",
-      transactionDate: "05/24/20202",
+      toAccount: "HS10002",
+      transactionDate: "06/10/2020",
       amount: "1000"
     },
 
@@ -20,7 +20,7 @@
       fromAccount: "SV10002",
       toAccount: "HS10001",
       toUser: "Sam",
-      transactionDate: "06/12/20202",
+      transactionDate: "06/10/2020",
       amount: "1200"
     },
 
@@ -29,7 +29,7 @@
       fromAccount: "SV10001",
       toAccount: "HS10023",
       toUser: "Car Dealer",
-      transactionDate: "06/01/20202",
+      transactionDate: "06/01/2020",
       amount: "1500"
     },
 
@@ -38,9 +38,76 @@
       fromAccount: "SV10002",
       toAccount: "HS10043",
       toUser: "Jassica",
-      transactionDate: "06/01/20202",
+      transactionDate: "06/01/2020",
       amount: "2000"
-    }
+    },
+
+    {
+      type: "One time",
+      fromAccount: "CRD10008",
+      toAccount: "HS10043",
+      toUser: "TV Store",
+      transactionDate: "07/10/2020",
+      amount: "2000"
+    },
+
+    {
+      type: "One time",
+      fromAccount: "CRD10008",
+      toAccount: "Payee1001",
+      toUser: "Food",
+      transactionDate: "08/10/2020",
+      amount: "2000"
+    },
+
+    {
+      type: "One time",
+      fromAccount: "CRD10008",
+      toAccount: "HS10043",
+      toUser: "Food",
+      transactionDate: "08/10/2020",
+      amount: "2000"
+    },
+
+
+    {
+      type: "One time",
+      fromAccount: "CRD10008",
+      toAccount: "Pyee10009",
+      toUser: "Fussion",
+      transactionDate: "08/10/2020",
+      amount: "2000"
+    },
+
+    {
+      type: "One time",
+      fromAccount: "CRD10008",
+      toAccount: "Payee10001",
+      toUser: "Maket Place",
+      transactionDate: "10/10/2020",
+      amount: "2000"
+    },
+
+
+    {
+      type: "One time",
+      fromAccount: "CRD10009",
+      toAccount: "Payee10001",
+      toUser: "Maket Place",
+      transactionDate: "10/10/2020",
+      amount: "2000"
+    },
+
+    {
+      type: "One time",
+      fromAccount: "CRD10009",
+      toAccount: "Payee10001",
+      toUser: "Maket Place",
+      transactionDate: "09/10/2020",
+      amount: "2000"
+    },
+
+
   ];
 
   module.exports = transactions;
